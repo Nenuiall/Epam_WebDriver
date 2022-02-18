1be72170c07f7f16fafa81b71106eec60e0e52bb
Nenuiall <95130542+Nenuiall@users.noreply.github.com>
2022-02-18 13:52:01 +0300

    ignore

---
+++ helpers/waits.js
@@ -0,0 +1,13 @@
+const {Builder, By, until} = require('selenium-webdriver');
+driver = require('../tests_samples/BringItOn.spec');
+
+class myWaits {
+    async waitbyClN(myClass, time) {
+        await driver.wait(until.elementLocated(By.className(myClass)),time);
+      };
+    async waitbyXp(myXpath, time) {
+        await driver.wait(until.elementLocated(By.xpath(myXpath)),time);
+      };
+};
+
+module.exports = new myWaits();
