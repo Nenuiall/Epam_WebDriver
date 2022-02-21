99dd0fb9e04a7408ba051c9026ab7715be895b1f
Nenuiall <95130542+Nenuiall@users.noreply.github.com>
2022-02-21 10:45:10 +0300

    hardcore

--- test/specs/Hardcore.spec.js
+++ test/specs/Hardcore.spec.js
@@ -6,7 +6,6 @@ const {Builder, until} = require('selenium-webdriver');
 const {before, after} = require('mocha');
 const chai = require('chai');
 const {expect, assert} = require('chai');
-const yopmailBoxPage = require('../../pages/yopmailBox.page');
 
 
 describe('Google cloud test', async function() {  
@@ -32,7 +31,7 @@ describe('Google cloud test', async function() {
       await PricingCalculatorPage.clickEmailSendBtn();
       await driver.navigate().to(urlOfYopmailBox);
       await driver.sleep(10000);
-      await driver.switchTo().frame(yopmailBoxPage.mailFrame);
+      await driver.switchTo().frame(YopmailBoxPage.mailFrame);
 
       let expectedEstimatedCost = 'USD 1,082.77';
       let estimatedCostFromYopmailBox = await driver.wait(until.elementIsVisible(YopmailBoxPage.estimatedMonthlyCost), 1000).getText();
